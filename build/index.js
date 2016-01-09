ae70a2eb6e22ada3c0fe2345974e1f6540a95c4d
Tan Nguyen <tan@sharkpunch.com>
2016-01-09 10:13:58 +0200

    Update gulp-tasks

--- build/index.js
+++ build/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var _ = require('lodash');
 
-var DbCriteria = (function () {
+var DbCriteria = function () {
   function DbCriteria(criteria) {
     _classCallCheck(this, DbCriteria);
 
@@ -155,7 +155,7 @@ var DbCriteria = (function () {
   }]);
 
   return DbCriteria;
-})();
+}();
 
 var comparisons = ['gt', 'gte', 'lt', 'lte', 'neq', 'in', 'nin'];
 
